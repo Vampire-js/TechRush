c6d608d1914c7de0ea5eb4e34384e21b29ef4b3d
Vampire.js <vampirejs.dev@gmail.com>
2025-08-04 22:56:51 +0530

    Made the knob movements more polished

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 const knob = document.getElementById("knob")
 
-const calculateRotation = (dist , speed) => {
-    return speed * dist/2;
+const calculateRotation = (dist, speed) => {
+    return speed * dist / 2;
 }
 
-knob.onclick = () => {
+let isDragging = false
+let lastY = 0
+let rotation = 0
+knob.onpointerdown = e => {
+    isDragging = true
+    lastY = e.clientY
+    knob.setPointerCapture(e.pointerId)
+}
 
-    knob.addEventListener("mousemove", e => {
-        knob.style.transform = `rotate(${calculateRotation(e.clientX, 5)}deg)`;
-    })
+knob.onpointermove = (e) => {
+    if(!isDragging) return
+    const deltaY = lastY - e.clientY
+    rotation += deltaY * .5;
+    rotation = Math.max(0, Math.min(270, rotation))
+
+    knob.style.transform = `rotate(${rotation}deg)`
+    lastY = e.clientY
+}
+
+knob.onpointerup = () => {
+    isDragging = false
 }
